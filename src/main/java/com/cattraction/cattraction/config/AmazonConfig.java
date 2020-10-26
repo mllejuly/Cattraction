34f3ed5c91ad75b85f098c98be43120e9d121ac0
Erin Fang <qifangerin@gmail.com>
2020-10-26 00:24:21 -0400

    Update AmazonConfig.java

--- src/main/java/com/cattraction/cattraction/config/AmazonConfig.java
+++ src/main/java/com/cattraction/cattraction/config/AmazonConfig.java
@@ -14,8 +14,8 @@ public class AmazonConfig {
     @Bean
     public AmazonS3 s3() {
         AWSCredentials awsCredentials = new BasicAWSCredentials(
-                "AKIAJFB5AO5BE7UMDROQ",
-                "qc1biQ3xtiVyvRSMb4jzUZ/BtCxlBpipmlENeGvs%"
+                "***********************",
+                "***********************"
         );
         return AmazonS3ClientBuilder
                 .standard()
